d0836e5493af5001d738d97ff7a5383f63acbf84
Mensah Kwame Anni <mkanni@st.ug.edu.gh>
2024-12-30 01:07:13 +0000

    Update Motorcycle.java

--- Motorcycle.java
+++ Motorcycle.java
@@ -1,1 +1,44 @@
 
+public class Motorcycle extends Vehicle {
+    private boolean needsHelmet;//indicates helmet is provided
+    private boolean hasInsurance;//indicates there is insurance cover
+
+  //constructor
+    public Motorcycle(String vehicleID, String model, double baseRentalRate, boolean needsHelmet, boolean hasInsurance) {
+        super(vehicleID, model, baseRentalRate);
+        this.needsHelmet = needsHelmet; 
+        this.hasInsurance = hasInsurance;
+    }
+
+  
+    public boolean needsHelmet() { 
+        return needsHelmet; 
+    }
+
+    public void setNeedsHelmet(boolean needsHelmet) { 
+        this.needsHelmet = needsHelmet;
+    }
+
+    public boolean hasInsurance() { 
+        return hasInsurance;
+    }
+
+    public void setHasInsurance(boolean hasInsurance) {
+        this.hasInsurance = hasInsurance; 
+    }
+
+    @Override 
+    public double calculateRentalCost(int days) { 
+        return getBaseRentalRate() * days; // Rental cost calculation
+    }
+
+    @Override
+    public boolean isAvailableForRental() {
+        return isAvailable(); 
+    }
+
+    @Override
+    public String toString() {
+        return "Motorcycle: " + getModel() + " (ID: " + getVehicleID() + ")";
+    }
+}
