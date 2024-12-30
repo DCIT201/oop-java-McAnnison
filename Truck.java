057ed7639425ebc7d7d9fb4d60b0780112e9e92d
Mensah Kwame Anni <mkanni@st.ug.edu.gh>
2024-12-30 01:09:16 +0000

    Update Truck.java

--- Truck.java
+++ Truck.java
@@ -1,1 +1,32 @@
 
+public class Truck extends Vehicle {
+    public Truck(String vehicleID, String moedl, double baseRentalRate, double loadCapacity) {
+        super(vehicleID, model, baseRentalRate);
+        this.loadCapacity = loadCapacity;
+    }
+
+    public double getLoadCapacity() {
+        return loadCapacity;
+    }
+
+    public void setLoadCapacty(double loadCapacity) {
+        this.loadCapacity = loadCapacity;
+    }
+
+    //calculate rental cost + charges fro cargo or load
+    @Override
+    public double calculateRentalCost(int days) {
+        return getBaseRentalRate() * days + loadCapacity * 2;
+    } 
+
+    @Override 
+    public boolean isAvailableForRental() {
+        return isAvailable();
+    }
+
+    @Override
+    public String toString() {
+        return "Truck: " + getModel() + "(ID: " + getVehicleID() + ") ";
+    }
+
+}
