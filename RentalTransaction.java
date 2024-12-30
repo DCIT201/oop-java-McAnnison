1cb834f757144c105d7cc3fd9c54f903d7120039
Mensah Kwame Anni <mkanni@st.ug.edu.gh>
2024-12-30 01:33:12 +0000

    Update RentalTransaction.java

--- RentalTransaction.java
+++ RentalTransaction.java
@@ -1,1 +1,24 @@
 
+public class RentalTransaction {
+    private String transactionID;
+    private Vehicle vehicle;
+    private Customer customer;
+    private int daysRented;
+
+    public RentalTransaction(String transactionID, Vehicle vehicle, Customer customer, int daysRented) {
+        this.transactionID = transactionID;
+        this.vehicle = vehicle;
+        this.customer = customer;
+        this.daysRented = daysRented;
+    }
+
+    // to calculte the total cost of the rental
+    public double calculateTotalCost() {
+        return vehicle.calculateRentalCost(daysRented);
+    }
+
+@Override
+public String toString() {
+    return "Transaction ID: " + transactionID + ", Vehicle rented: " + getModel() + ", Customer: " + customer.getName() + ", The Total Cost:" + calculateTotalCost() ;
+}
+}
